041005c03c9d3a5c05d03010a7ca87752001bf3b
Al3Dev <alejandro.rodriguez1753@alumnos.udg.mx>
2024-09-04 09:53:49 -0600

    p5 log in

--- mechmake.py
+++ mechmake.py
@@ -5,6 +5,16 @@ mechmake = Flask(__name__)
 @mechmake.route('/')
 def home():
     return render_template('home.html')
+    
+@mechmake.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+
+@mechmake.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 
 if __name__ == '__main__':
       mechmake.run(debug=True,port=3300)
