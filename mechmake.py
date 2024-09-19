1630f227475149b2d89d7dcb603f3a043a2a0331
Al3Dev <alejandro.rodriguez1753@alumnos.udg.mx>
2024-09-19 16:23:24 -0600

    m13

--- mechmake.py
+++ mechmake.py
@@ -19,19 +19,19 @@ def signin():
 @mechmake.route('/signup', methods =['POST','GET'])
 def signup():
     if request.method == 'POST':
-         nombre = request.form['nombre']
-         correo = request.form['correo']
-         clave = request.form['clave']
-         claveCifrado = generate_password_hash(clave)
-         fechaReg = datetime.datetime.now()
-         regUsuario = db.connection.cursor()
-         regUsuario.execute("INSERT INTO usuario (nombre,  correo, clave,fechareg) VALUES (%s, %s, %s, %s)", (nombre,correo,claveCifrado,fechaReg))
-         db.connection.commit()
-         return render_template('home.html')
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        clave = request.form['clave']
+        claveCifrado = generate_password_hash(clave)
+        fechaReg = datetime.datetime.now()
+        regUsuario = db.connection.cursor()
+        regUsuario.execute("INSERT INTO usuario (nombre,  correo, clave,fechareg) VALUES (%s, %s, %s, %s)", (nombre,correo,claveCifrado,fechaReg))
+        db.connection.commit()
+        return render_template('home.html')
     else:
-         return render_template('signup.html')
+        return render_template('signup.html')
 
 
 if __name__ == '__main__':
-      mechmake.config.from_object(config['development'])
-      mechmake.run(port=3300)
+    mechmake.config.from_object(config['development'])
+    mechmake.run(port=3300)
